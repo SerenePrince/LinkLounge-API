d8cc67d61ab90298a618874c2ff155baa059e4fd
Noah Park-Nguyen <noahparknguyen@gmail.com>
2025-01-10 11:58:40 -0500

    updated path

--- config/allowedOrigins.js
+++ config/allowedOrigins.js
@@ -1,5 +1,5 @@
 const allowedOrigins = [
-  "https://linklounge-2inr.onrender.com/", // Render url environment
+  "https://linklounge-2inr.onrender.com", // Render url environment
 ];
 
 module.exports = allowedOrigins;
